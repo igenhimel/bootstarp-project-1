4d9102b1ab7b3f0be0ac8ee9aa8b6c3638a888a7
igenhimel <djstar.himel@gmail.com>
2021-03-25 21:01:20 +0600

    updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
+    $(".features-1").owlCarousel({
        
        autoplay:true,
        loop:true,
@@ -24,4 +24,66 @@ $(document).ready(function () {
        smartSpeed:400
 
     });
+
+
+
+    $(".apps-ss").owlCarousel({
+       
+        autoplay:false,
+        loop:true,
+        items:4,
+        margin:0,
+        responsiveClass:true,
+        responsiveRefreshRate:true,
+        responsive:{
+            0:{
+                items:1,
+                autoplay:true
+            },
+ 
+            768:{
+                items:2,
+                autoplay:true
+            },
+            992:{
+                items:3,
+                autoplay:true
+            },
+            1200:{
+                items:4
+            }
+        },
+        smartSpeed:400
+ 
+     });
+
+     $(".client-carousel").owlCarousel({
+       
+        autoplay:false,
+        loop:true,
+        items:3,
+        margin:0,
+        responsiveClass:true,
+        responsiveRefreshRate:true,
+        responsive:{
+            0:{
+                items:1,
+                autoplay:true
+            },
+ 
+            768:{
+                items:2,
+                autoplay:true
+            },
+            992:{
+                items:3,
+                autoplay:true
+            },
+           
+        },
+        smartSpeed:400
+ 
+     });
+
+   
 });
